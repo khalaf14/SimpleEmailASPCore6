ec59a03660a60007ea1de43a935359821b90fe8a
khalaf14 <73171806+khalaf14@users.noreply.github.com>
2022-07-11 15:34:48 +0300

    Add Attachment To email send

--- SimpleEmailApp/Controllers/EmailController.cs
+++ SimpleEmailApp/Controllers/EmailController.cs
@@ -2,6 +2,7 @@
 using MailKit.Security;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using MimeKit;
 using MimeKit.Text;
 
@@ -23,6 +24,38 @@ namespace SimpleEmailApp.Controllers
         {
             _emailService.SendEmail(request);
             return Ok();
+        }
+
+        [HttpGet("Download")]
+        // you can make file name as a parameter of the api request aka. getFileAsync(string fname)
+        public async Task<IActionResult> getFileAsync(bool IsImg, string? fileToDownload = null)
+        {
+            //Byte[] b;
+            if (IsImg)
+            {
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToget = Path.Combine(Directory.GetCurrentDirectory(), folderName,fileToDownload ?? "204041712_72027270.jpg");
+                var image = await System.IO.File.ReadAllBytesAsync(pathToget);
+                return File(image, "image/jpg");
+            }
+            else
+            {
+                
+                try
+                {
+                    var folderName = Path.Combine("Resources", "Files");
+                    var FullPathWuthFile = Path.Combine(Directory.GetCurrentDirectory(), folderName, fileToDownload ?? "excel_list.xlsx");
+                    var content = await System.IO.File.ReadAllBytesAsync(FullPathWuthFile);
+                    var fileName = System.IO.Path.GetFileName(FullPathWuthFile);
+                    new FileExtensionContentTypeProvider()
+                        .TryGetContentType(fileName, out string contentType);
+                    return File(content, contentType, fileName);
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+            }
         }
     }
 }
--- SimpleEmailApp/Services/EmailService/EmailService.cs
+++ SimpleEmailApp/Services/EmailService/EmailService.cs
@@ -1,7 +1,9 @@
 ﻿using MailKit.Net.Smtp;
 using MailKit.Security;
+using Microsoft.AspNetCore.StaticFiles;
 using MimeKit;
 using MimeKit.Text;
+using System.Diagnostics;
 
 namespace SimpleEmailApp.Services.EmailService
 {
@@ -16,11 +18,28 @@ namespace SimpleEmailApp.Services.EmailService
 
         public void SendEmail(EmailDto request)
         {
+            Debug.WriteLine($"value of {nameof(request.To)} is {request.To}");
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
             email.To.Add(MailboxAddress.Parse(request.To));
             email.Subject = request.Subject;
             email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+            // email.Attachments.
+            var builder = new BodyBuilder();
+
+            // Set the plain-text version of the message text
+            builder.HtmlBody = request.Body;
+
+            // We may also want to attach a calendar event for Monica's party...
+            var folderName = Path.Combine("Resources", "Files");
+            var FullPathWuthFile = Path.Combine(Directory.GetCurrentDirectory(), folderName, "excel_list.xlsx");
+
+            builder.Attachments.Add(FullPathWuthFile);
+
+            // Now we just need to set the message body and we're done
+            email.Body = builder.ToMessageBody();
+            // email attachments
 
             using var smtp = new SmtpClient();
             smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
@@ -28,5 +47,7 @@ namespace SimpleEmailApp.Services.EmailService
             smtp.Send(email);
             smtp.Disconnect(true);
         }
+
+
     }
 }
